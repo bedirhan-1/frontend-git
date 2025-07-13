4e1ffbd5672281dce6d9756aab90875bf42234e2
bedirhan-1 <bedirhan9828@gmail.com>
2025-07-13 16:29:15 +0300

    refactor: :art: check user inputs if its valid
    check if user input (eg email & password) valid like if password shorter than 6 character them show errors and same for email

--- login.js
+++ login.js
@@ -3,3 +3,7 @@
 // Bilgileri kontrol ediliyor...
 
 // <kodlar>
+// yeni eklenilen bazı kodlar. Bu kodlar kullanıcı
+// girdilerinin içerik kontrolünü yapan ve
+// yanlış bir email veya eksik karakterli bir password'ün
+// uyarı bilgilerini kullanıcıya bildiren kod
